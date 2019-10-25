74796010a5b02f85cdc8bce0bbb6c90bb8d8d288
Muhammad Imran <ilyasimran27@gmail.com>
2019-10-25 15:42:32 -0700

    Updates

---
+++ precache-manifest.7e8758102f746c69992f83ba92269b1d.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "3a690cbbbc91085654f6e23fe6a9969e",
+    "url": "/personapp/index.html"
+  },
+  {
+    "revision": "90dcdacd440e6261835e",
+    "url": "/personapp/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "e5db901d658f02316940",
+    "url": "/personapp/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "90dcdacd440e6261835e",
+    "url": "/personapp/static/js/2.d1d79015.chunk.js"
+  },
+  {
+    "revision": "e5db901d658f02316940",
+    "url": "/personapp/static/js/main.15d9060b.chunk.js"
+  },
+  {
+    "revision": "0bc09683146beb2e1ab3",
+    "url": "/personapp/static/js/runtime-main.f9eabe6d.js"
+  },
+  {
+    "revision": "25bf045ca257e971124f3997d89f321c",
+    "url": "/personapp/static/media/logo.25bf045c.svg"
+  }
+]);
